4a4755bec1f88b46b4a986c4d285272f77ccf964
Bybeez <j.foucaud@outlook.fr>
2019-06-24 13:50:51 +0200

    corrected stuff

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -39,7 +39,7 @@ class IndexController extends Controller
             abort(403);
         }
 
-        $columns = DatasetController::getAllAccessibleColumnsFromADataset($request, $datasetId);
+        $columns = DatasetController::getAllAccessibleColumnsFromADataset($request, dataset::where('id', $datasetId));
         $columnFilter = [];
         foreach($columns as $column){
             array_push($columnFilter, $column->name);
