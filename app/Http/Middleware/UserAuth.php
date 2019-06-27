90d8b0fa9a3bb38d9e585e10a637418e4267a0c8
Esteban Lhote <esteban.lhote@outlook.fr>
2019-06-27 10:35:12 +0200

    Cas commit

--- app/Http/Middleware/UserAuth.php
+++ app/Http/Middleware/UserAuth.php
@@ -2,8 +2,11 @@
 
 namespace App\Http\Middleware;
 use App\user;
+use GuzzleHttp\Client;
 
 use Closure;
+use PHPUnit\Util\Json;
+use function GuzzleHttp\json_decode;
 
 class UserAuth
 {
@@ -22,19 +25,33 @@ class UserAuth
             abort(401);
         }
 
-        if($token == "Juiploetdjtozvelnjzkfpofn"){
-            $user = user::where('uuid',"2be8c158-29a7-42b3-a9fb-de9ec266e196")->first();
+        $user = user::where('token',$token)->first();
+        if($user != null){
+            $request->merge(['user' => $user]);
+            return $next($request);
+        }
+
+        $client = new Client();
+        $headers = [
+            'Authorization' => 'Bearer ' . $token,
+            'Accept'        => 'application/json',
+        ];
+        $res = $client->get('https://authsso.extranet.toulouse.fr/cas/oidc/profile', ['headers' => $headers]);
+        $data = json_decode($res->getBody(),true);
+        $value = $data["sub"];
+        if(!isset($value)){
+            abort(403);
+        }
+
+            $user = user::where('uuid',$value)->first();
             if($user == null){
                 abort(403);
             }
             else{
+                $user->token = $value;
                 $request->merge(['user' => $user]);
 
             }
-        }
-        else{
-            abort(403);
-        }
 
         return $next($request);
     }
