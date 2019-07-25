a7960c1853ea9eaa31517cbec1a7902c1f1dcda0
Bybeez <j.foucaud@outlook.fr>
2019-07-25 15:29:39 +0200

    debug

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -121,7 +121,7 @@ class IndexController extends Controller
     {
         $user = $request->get('user');
         $canAccess = false;
-        $datasets = DatasetController::getAllAccessibleDatasets($request, $user, true);
+        $datasets = DatasetController::getAllAccessibleDatasets($request, $user, false);
         foreach ($datasets as $dataset) {
             if ($name === $dataset->databaseName) {
                 $datasetId = $dataset->id;
