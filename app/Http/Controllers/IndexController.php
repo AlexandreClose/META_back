e0f84e647c289c0a052464f5de69f68d0ff106b9
Bybeez <j.foucaud@outlook.fr>
2019-07-25 16:32:31 +0200

    debug

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -163,7 +163,7 @@ class IndexController extends Controller
 
         foreach($accessibleFields as $acc_field) {
             foreach($fields as $field){
-                dd(json_encode($acc_field));
+                dd(json_encode($acc_field['column']));
                 if($field[1] == $acc_field->attributes['name']){
                     array_push($results, $field);
                 }
