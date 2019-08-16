6ba7f11980f0284e5fb019fd669b9a799ae716b1
Bybeez <j.foucaud@outlook.fr>
2019-08-16 17:31:40 +0200

    corrected stupid stuff

--- app/Http/Controllers/AnalyseController.php
+++ app/Http/Controllers/AnalyseController.php
@@ -33,6 +33,7 @@ class AnalyseController extends Controller
             error_log("missing theme");
             abort(400, "missing theme or theme don't exist");
         }
+        $analyse->theme_name = $theme_name;
         error_log($analyse->visibility);
         $analyse->save();
         
