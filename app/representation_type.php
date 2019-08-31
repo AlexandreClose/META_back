35993df9cd608dd9aed56af8fc9e9e3863bc1644
Bybeez <j.foucaud@outlook.fr>
2019-08-31 16:40:22 +0200

    worked on getRep of dataset

--- app/representation_type.php
+++ app/representation_type.php
@@ -9,6 +9,7 @@ class representation_type extends Model
     protected $primaryKey = 'name';
     public $incrementing = false;
     protected $keyType = 'string';
+    
     public function datasets(){
         return $this->belongsToMany('App\dataset','dataset_has_represenation','datasetId','representationName');
     }
