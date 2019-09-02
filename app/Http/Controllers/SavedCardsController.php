fd8f7ab34841cd4af1271b64d09ee8285f418b9c
Bybeez <j.foucaud@outlook.fr>
2019-09-02 11:58:35 +0200

    changed stuff

--- app/Http/Controllers/SavedCardsController.php
+++ app/Http/Controllers/SavedCardsController.php
@@ -12,8 +12,9 @@ class SavedCardsController extends Controller
         $user = $request->get('user');
         $saved_cards = saved_card::where('user_uuid', $user->uuid)->get();
         foreach ($saved_cards as $saved_card) {
-            $saved_card->analysis = analysis::where('id', $saved_card->analysis_id);
-            $saved_card->analysis->analysis_column = $saved_card->analysis->analysis_columns;
+            $analysis = analysis::where('id', $saved_card->analysis_id);
+            $saved_card->analysis = $analysis;
+            $saved_card->analysis->analysis_column = $analysis->analysis_columns;
         }
 
         return response($saved_cards)->header('Content-Type', 'application/json')->header('charset', 'utf-8');
