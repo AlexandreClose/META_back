abb255ea65ac288b02214e65b9c7e90bb963775b
Bybeez <j.foucaud@outlook.fr>
2019-09-02 11:48:50 +0200

    changed stuff

--- app/saved_card.php
+++ app/saved_card.php
@@ -9,11 +9,11 @@ class saved_card extends Model
     
     public function user()
     {
-        return $this->hasOne('App\user', 'uuid', 'uuid');
+        return $this->hasOne('App\user', 'user_uuid', 'uuid');
     }
 
     public function analysis()
     {
-        return $this->hasOne('App\analysis', 'id', 'id');
+        return $this->hasOne('App\analysis', 'analysis_id', 'id');
     }
 }
