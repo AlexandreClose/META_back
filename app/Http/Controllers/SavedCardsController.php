fbf31db71865b503af35c2ac6b300a0a457f30ed
Bybeez <j.foucaud@outlook.fr>
2019-09-02 11:33:32 +0200

    undo test

--- app/Http/Controllers/SavedCardsController.php
+++ app/Http/Controllers/SavedCardsController.php
@@ -21,7 +21,6 @@ class SavedCardsController extends Controller
         $user = $request->get('user');
 
         $card = new saved_card();
-        return $request;
         $card->analysis_id = $request->get('id');
         $card->user_uuid = $user->uuid;
         $card->position = saved_card::where('user_uuid', $user->uuid)->count();
